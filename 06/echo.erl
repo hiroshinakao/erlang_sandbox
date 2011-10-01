4179fed9fff56a21b1f6a89733c2ec87d4fa6cf4
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2011-10-01 23:47:08 +0900

    echo.erlをリファクタリング

--- 06/echo.erl
+++ 06/echo.erl
@@ -1,29 +1,13 @@
 -module(echo).
 -export([start/0, print/1, stop/0, loop/0]).
 
-start() ->
-  register(echo, spawn(echo, loop, [])),
-  echo ! start
-.
+start() -> register(?MODULE, spawn(echo, loop, [])), ok.
 
-print(Term) ->
-  echo ! {print, Term}
-.
-
-stop() ->
-  echo ! stop
-.
+print(Term) -> ?MODULE ! {print, Term}, ok.
+stop()      -> ?MODULE ! stop         , ok.
 
 loop() ->
   receive
-    {print, Msg} ->
-      io:format("Msg: ~w~n",[Msg]),
-      loop();
-    start ->
-      io:format("[start]echo server ~n", []),
-      loop();
-    stop ->
-      io:format("[stop]echo server ~n", []),
-      true
-  end
-.
+    {print, Msg} -> io:format("Msg: ~w~n",[Msg]), loop();
+    stop         -> ok
+  end.
