2750ec110bb5bb09d2cf34591d333ed82e3d9e7e
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2011-10-02 00:04:58 +0900

    echo.erlのstopメソッドでexit関数を呼び出すように変更

--- 06/echo.erl
+++ 06/echo.erl
@@ -1,13 +1,11 @@
 -module(echo).
 -export([start/0, print/1, stop/0, loop/0]).
 
-start() -> register(?MODULE, spawn(echo, loop, [])), ok.
-
-print(Term) -> ?MODULE ! {print, Term}, ok.
-stop()      -> ?MODULE ! stop         , ok.
+start()     -> register(echo, spawn_link(?MODULE, loop, [])), ok.
+print(Term) -> echo ! {print, Term}, ok.
+stop()      -> exit(normal).
 
 loop() ->
   receive
-    {print, Msg} -> io:format("Msg: ~w~n",[Msg]), loop();
-    stop         -> ok
+    {print, Term} -> io:format("Term: ~w~n",[Term]), loop()
   end.
