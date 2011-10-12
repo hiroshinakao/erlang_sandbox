2566350b3484ae6e0070c66f8df9dd897cfa4fc9
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2011-10-12 11:10:34 +0900

    07/db.erlをrecordを使うようにしました

--- 07/db.erl
+++ 07/db.erl
@@ -2,19 +2,21 @@
 -export([new/0, destroy/1]).
 -export([write/3, delete/2, read/2, match/2]).
 
+-record(data, {key, data}).
+
 new() -> [].
 destroy(_Db) -> ok.
 
-write(Key, Element, Db) -> [{Key, Element} | Db].
+write(Key, Element, Db) -> [#data{key=Key, data=Element} | Db].
 
 delete(_Key, []) -> [];
-delete(Key, [{K,_E}|Tail]) when Key =:= K -> delete(Key, Tail);
+delete(Key, [#data{key=Key}|Tail]) -> delete(Key, Tail);
 delete(Key, [Head|Tail]) -> [Head | delete(Key, Tail)].
 
 read(_Key, []) -> {error, instance};
-read(Key, [{K,E}|_Tail]) when Key =:= K -> {ok, E};
+read(Key, [#data{key=Key,data=E}|_Tail]) -> {ok, E};
 read(Key, [_Head|Tail]) -> read(Key, Tail).
 
 match(_Element, []) -> [];
-match(Element, [{K,E}|Tail]) when Element =:= E -> [K | match(Element, Tail)];
+match(Element, [#data{key=K,data=Element}|Tail]) -> [K | match(Element, Tail)];
 match(Element, [_Head|Tail]) -> match(Element, Tail).
