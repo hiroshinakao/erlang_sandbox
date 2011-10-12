aeec623a5c923ca47e668df5d3131a8865cf2116
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2011-10-12 11:41:53 +0900

    円の半径の引数に制限を追加

--- 07/figure.erl
+++ 07/figure.erl
@@ -4,6 +4,7 @@
 
 %% 円
 -record(circle, {radius=0}).
-circle(Radius)                        -> #circle{radius=Radius}.
+circle(Radius) when Radius > 0        -> #circle{radius=Radius};
+circle(Radius)                        -> {error, wrong_arguments}.
 circumference(#circle{radius=Radius}) -> Radius * 2 * math:pi().
 area(#circle{radius=Radius})          -> Radius * Radius * math:pi().
