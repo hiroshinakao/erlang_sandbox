0bbf5e14c64623ef43e01215d0091f6084d35e97
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2011-10-16 22:36:24 +0900

    recordの形式を決定

--- 07/binary_tree.erl
+++ 07/binary_tree.erl
@@ -1,1 +1,19 @@
 -module(binary_tree).
+-export([sum/1]).
+
+-record(binary_tree, {value=0, left=[], right=[]}).
+%          |- 4
+%    |- 2 -|
+%    |     |- 5
+% 1 -|
+%    |- 3
+%
+% #binary_tree{
+%   value=1,
+%   right=#binary_tree{
+%     value=2,
+%     left =#binary_tree{value=3},
+%     right=#binary_tree{value=4}
+%   },
+%   left=#binary_tree{value=3}
+% }
