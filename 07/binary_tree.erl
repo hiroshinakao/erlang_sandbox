9c2add7de33fe43dd8e6f3162faf5bd08aea31c0
Hiroshi Nakao <hiroshi.nakao@gmail.com>
2011-10-17 00:31:32 +0900

    is_ordered/1の実装

--- 07/binary_tree.erl
+++ 07/binary_tree.erl
@@ -1,29 +1,30 @@
 -module(binary_tree).
 -export([sum/1, max/1]).
+-export([is_ordered/1]).
 -export([new/0]).
 
 -record(binary_tree, {value=0, left=[], right=[]}).
-%          |- 4
-%    |- 2 -|
-%    |     |- 5
-% 1 -|
-%    |- 3
+%          |- 5
+%    |- 4 -|
+%    |     |- 3
+% 3 -|
+%    |- 2
 %
 % #binary_tree{
-%   value=1,
+%   value=3,
 %   right=#binary_tree{
-%     value=2,
-%     left =#binary_tree{value=4},
-%     right=#binary_tree{value=5}},
-%   left=#binary_tree{value=3}}
+%     value=4,
+%     right=#binary_tree{value=5},
+%     left =#binary_tree{value=3}},
+%   left=#binary_tree{value=2}}
 
 new() -> #binary_tree{
-    value=1,
+    value=3,
     right=#binary_tree{
-      value=2,
-      left =#binary_tree{value=4},
-      right=#binary_tree{value=5}},
-    left=#binary_tree{value=3}}.
+      value=4,
+      right=#binary_tree{value=5},
+      left =#binary_tree{value=3}},
+    left=#binary_tree{value=2}}.
 
 % sum
 sum([]) -> 0;
@@ -32,3 +33,23 @@ sum(#binary_tree{value=Value, left=Left, right=Right}) -> Value + sum(Left) + su
 % max
 max([]) -> 0;
 max(#binary_tree{value=Value, left=Left, right=Right}) -> lists:max([Value, max(Left), max(Right)]).
+
+% is_ordered
+is_ordered([]) -> true;
+is_ordered(#binary_tree{value=Value, left=Left, right=Right}) ->
+  io:format("val:~w, left:~w, right:~w~n", [Value, value(Left), value(Right)]),
+  LeftResult = case is_record(Left, binary_tree) of
+    true  -> value(Left) =< Value;
+    false -> true
+  end,
+  RightResult = case is_record(Right, binary_tree) of
+    true  -> Value =< value(Right);
+    false -> true
+  end,
+  case LeftResult and RightResult of
+    true  -> is_ordered(Left) and is_ordered(Right);
+    false -> false
+  end.
+
+value([]) -> 0;
+value(#binary_tree{value=Value, left=_Left, right=_Right}) -> Value.
